ce25b63119f11be6ac374b6666f386321e63a945
Irinaabieva <irinaabieva03@gmail.com>
2022-10-31 16:14:37 +0300

    Задание 6

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес бойцов " + totalWeight);
+        var differenceWeight = firstBoxer - secondBoxer;
+        System.out.println("Разница между весами бойцов " + differenceWeight);
     }
 }
